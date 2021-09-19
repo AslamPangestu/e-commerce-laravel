0d76dab19fb417cd54d07f854a3bc6d03e900e7b
aslampangestu <aslampangestu@gmail.com>
2021-09-19 11:08:15 +0700

    done login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\API;
 
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use App\Repositories\Interfaces\UserRepositoryInterfaces;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Fortify\Rules\Password;
 
 class AuthController extends Controller
@@ -62,6 +63,47 @@ class AuthController extends Controller
             return $this->error([
                 'message' => 'Something went wrong',
                 'error' => $error->getMessage(),
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return $this->error([
+                    'message' => 'Unauthorized'
+                ], 'Authentication Failed', 500);
+            }
+
+            $user = $this->repository->findOneByQuery([
+                ['email', '=', $request->email]
+            ]);
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new Exception('Invalid Credentials');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return $this->success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Authenticated');
+        } catch (Exception $error) {
+            return $this->error([
+                'message' => 'Something went wrong',
+                'error' => $error,
             ], 'Authentication Failed', 500);
         }
     }
